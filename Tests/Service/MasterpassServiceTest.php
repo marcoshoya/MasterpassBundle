d172b2e10bbc21368b2733460552832c61bfe439
Marcos Lazarin <marcoshoya@gmail.com>
2016-01-26 08:30:40 -0600

    initial test case

---
+++ Tests/Service/MasterpassServiceTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Hoya\MasterpassBundle\Tests\Service;
+
+/**
+ * MasterpassServiceTest
+ *
+ * @author Marcos Lazarin
+ */
+class MasterpassServiceTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+
+}
