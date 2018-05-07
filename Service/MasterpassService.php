fbfbd03c52c8db73ca47b9bc3415ee00140777e6
marcoshoya <marcoshoya@gmail.com>
2018-05-07 11:41:27 -0300

    removing legacy services

--- Service/MasterpassService.php
+++ Service/MasterpassService.php
@@ -4,23 +4,12 @@ namespace Hoya\MasterpassBundle\Service;
 
 use Hoya\MasterpassBundle\Common\Connector;
 use Hoya\MasterpassBundle\Common\BrandInterface;
-use Hoya\MasterpassBundle\DTO\AccessTokenResponse;
-use Hoya\MasterpassBundle\DTO\RequestTokenResponse;
 use Hoya\MasterpassBundle\DTO\CallbackResponse;
-use Hoya\MasterpassBundle\DTO\Transaction;
 
-class MasterpassService
+class MasterpassService 
 {
 
-    //Request Token Response
-    const XOAUTH_REQUEST_AUTH_URL = 'xoauth_request_auth_url';
-    const OAUTH_CALLBACK_CONFIRMED = 'oauth_callback_confirmed';
-    const OAUTH_EXPIRES_IN = 'oauth_expires_in';
-    //Request Token Response
-    const OAUTH_TOKEN_SECRET = 'oauth_token_secret';
-    const ORIGIN_URL = 'origin_url';
-    
-// Callback URL parameters
+    // Callback URL parameters
     const OAUTH_TOKEN = 'oauth_token';
     const OAUTH_VERIFIER = 'oauth_verifier';
     const CHECKOUT_RESOURCE_URL = 'checkout_resource_url';
@@ -29,37 +18,16 @@ class MasterpassService
     const PAIRING_VERIFIER = 'pairing_verifier';
     const CHECKOUTID = 'checkoutId';
     const MPSTATUS = 'mpstatus';
-    
-    // Redirect Parameters
-    const CHECKOUT_IDENTIFIER = 'checkout_identifier';
-    const ACCEPTABLE_CARDS = 'acceptable_cards';
-    const OAUTH_VERSION = 'oauth_version';
-    const VERSION = 'version';
-    const SUPPRESS_SHIPPING_ADDRESS = 'suppress_shipping_address';
-    const ACCEPT_REWARDS_PROGRAM = 'accept_reward_program';
-    const SHIPPING_LOCATION_PROFILE = 'shipping_location_profile';
-    const WALLET_SELECTOR = 'wallet_selector_bypass';
-    const DEFAULT_XMLVERSION = 'v1';
-    const AUTH_LEVEL = 'auth_level';
-    const BASIC = 'basic';
-    const XML_VERSION_REGEX = '/v[0-9]+/';
-    const REALM_TYPE = 'eWallet';
-    const APPROVAL_CODE = 'sample';
 
     /**
      * @var Connector
      */
     protected $connector;
-    
+
     /**
      * @var Hoya\MasterpassBundle\Common\BrandInterface
      */
     protected $brand;
-
-    /**
-     * @var RequestTokenResponse|null
-     */
-    protected $requestToken = null;
 
     /**
      * @param Connector $connector
@@ -79,7 +47,7 @@ class MasterpassService
     {
         return $this->connector;
     }
-    
+
     /**
      * Gets checkout id
      * 
@@ -89,7 +57,7 @@ class MasterpassService
     {
         return $this->brand->getCheckoutId();
     }
-    
+
     /**
      * Sets brand
      * 
@@ -100,177 +68,8 @@ class MasterpassService
     public function setBrand(BrandInterface $brand)
     {
         $this->brand = $brand;
-        
+
         return $this;
-    }
-
-    /**
-     * SDK:
-     * This method captures the Checkout Resource URL and Request Token Verifier
-     * and uses these to request the Access Token.
-     *
-     * @param string $requestToken
-     * @param string $verifier
-     *
-     * @return AccessTokenResponse
-     */
-    public function getAccessToken(CallbackResponse $callback)
-    {
-        $params = array(
-            self::OAUTH_VERIFIER => $callback->oauthVerifier,
-            self::OAUTH_TOKEN => $callback->oauthToken,
-        );
-
-        $accessToken = new AccessTokenResponse();
-        $response = $this->connector->doAccessToken($params);
-        $responseObject = $this->parseConnectionResponse($response);
-
-        $accessToken->accessToken = isset($responseObject[self::OAUTH_TOKEN]) ? $responseObject[self::OAUTH_TOKEN] : '';
-        $accessToken->oAuthSecret = isset($responseObject[self::OAUTH_TOKEN]) ? $responseObject[self::OAUTH_TOKEN_SECRET] : '';
-        $accessToken->checkoutResourceUrl = $callback->checkoutResourceUrl;
-
-        return $accessToken;
-    }
-
-    /**
-     * SDK:
-     * This method posts the Shopping Cart data to MasterCard services
-     * and is used to display the shopping cart in the wallet site.
-     *
-     * @param RequestTokenResponse $requestToken
-     * @param string               $shoppingCartXml
-     *
-     * @return string The XML response from MasterCard services
-     */
-    public function postShoppingCartData(RequestTokenResponse $requestToken, $shoppingCartXml)
-    {
-        $xml = simplexml_load_string($shoppingCartXml);
-        $xml->OAuthToken = $requestToken->requestToken;
-
-        $newShoppingCartXml = $xml->asXML();
-
-        $params = array(
-            Connector::OAUTH_BODY_HASH => $this->connector->generateBodyHash($newShoppingCartXml),
-        );
-
-        return $this->connector->doShoppingCart($params, $newShoppingCartXml);
-    }
-    
-    /**
-     * Posting Merchant init data do Masterpass services
-     * 
-     * @param RequestTokenResponse  $requestToken
-     * @param string                $initXml
-     * 
-     * @return string The XML response from MasterCard servicese
-     */
-    public function postMerchantInitData(RequestTokenResponse $requestToken, $initXml) 
-    {
-        $xml = simplexml_load_string($initXml);
-        $xml->OAuthToken = $requestToken->requestToken;
-        $xml->OriginUrl = $this->connector->getOriginUrl();
-
-        $newInitXml = $xml->asXML();
-
-        $params = array(
-            Connector::OAUTH_BODY_HASH => $this->connector->generateBodyHash($newInitXml),
-        );
-
-        return $this->connector->doMerchantInit($params, $newInitXml);
-    }
-
-    /**
-     * This method retrieves the payment and shipping information
-     * for the current user/session.
-
-     * @param AccessTokenResponse $accessToken
-     * 
-     * @return string The Checkout XML string containing the users billing and shipping information
-     */
-    public function getCheckoutData(AccessTokenResponse $accessToken)
-    {
-        $params = array(self::OAUTH_TOKEN => $accessToken->accessToken);
-
-        return $this->connector->doCheckoutData($params, $accessToken->checkoutResourceUrl);
-    }
-
-    /**
-     * This method submits the receipt transaction list to MasterCard as a final step
-     * in the Wallet process.
-     *
-     * @param Transaction $transaction
-     *
-     * @return string The XML response from MasterCard services
-     */
-    public function postTransaction(Transaction $transaction)
-    {
-        $xmlTransaction = $transaction->toXML();
-        $params = array(
-            Connector::OAUTH_BODY_HASH => $this->connector->generateBodyHash($xmlTransaction),
-        );
-
-        return $this->connector->doTransaction($params, $xmlTransaction);
-    }
-
-    /**
-     * @param string $preCheckoutUrl
-     * @param string $preCheckoutXml
-     * @param string $accessToken
-     *
-     * @return string The XML response from MasterCard services
-     */
-    public function getPreCheckoutData($preCheckoutUrl, $preCheckoutXml, $accessToken)
-    {
-        $params = array(
-            self::OAUTH_TOKEN => $accessToken,
-        );
-
-        return $this->connector->doRequest($params, $preCheckoutUrl, Connector::POST, $preCheckoutXml);
-    }
-
-    /**
-     * SDK:
-     * Get the user's request token and store it in the current user session.
-     * 
-     * @return RequestTokenResponse
-     */
-    public function getRequestToken()
-    {
-        $params = array(
-            Connector::OAUTH_CALLBACK => $this->connector->getCallbackUrl(),
-        );
-
-        $response = $this->connector->doRequestToken($params, null);
-        $requestTokenInfo = $this->parseConnectionResponse($response);
-
-        $requestToken = new RequestTokenResponse();
-        $requestToken->requestToken = isset($requestTokenInfo[self::OAUTH_TOKEN]) ? $requestTokenInfo[self::OAUTH_TOKEN] : '';
-        $requestToken->authorizeUrl = isset($requestTokenInfo[self::XOAUTH_REQUEST_AUTH_URL]) ? $requestTokenInfo[self::XOAUTH_REQUEST_AUTH_URL] : '';
-        $requestToken->callbackConfirmed = isset($requestTokenInfo[self::OAUTH_CALLBACK_CONFIRMED]) ? $requestTokenInfo[self::OAUTH_CALLBACK_CONFIRMED] : '';
-        $requestToken->oAuthExpiresIn = isset($requestTokenInfo[self::OAUTH_EXPIRES_IN]) ? $requestTokenInfo[self::OAUTH_EXPIRES_IN] : '';
-        $requestToken->oAuthSecret = isset($requestTokenInfo[self::OAUTH_TOKEN_SECRET]) ? $requestTokenInfo[self::OAUTH_TOKEN_SECRET] : '';
-
-        $this->requestToken = $requestToken;
-
-        return $requestToken;
-    }
-
-    /**
-     * Method used to parse the connection response and return a array of the data.
-     *
-     * @param $responseString
-     *
-     * @return array with all response parameters
-     */
-    private function parseConnectionResponse($responseString)
-    {
-        $token = array();
-        foreach (explode(Connector::AMP, $responseString) as $p) {
-            @list($name, $value) = explode(Connector::EQUALS, $p, 2);
-            $token[$name] = urldecode($value);
-        }
-
-        return $token;
     }
 
     /**
@@ -305,7 +104,7 @@ class MasterpassService
         $callback->checkoutResourceUrl = $request->get(self::CHECKOUT_RESOURCE_URL);
         $callback->oauthVerifier = $request->get(self::OAUTH_VERIFIER);
         $callback->oauthToken = $request->get(self::OAUTH_TOKEN);
-        
+
         return $callback;
     }
 
--- Tests/Service/MasterpassServiceTest.php
+++ Tests/Service/MasterpassServiceTest.php
@@ -20,12 +20,6 @@ use Hoya\MasterpassBundle\Service\MasterpassService;
  */
 class MasterpassServiceTest extends BaseWebTestCase
 {
-    const ACCESSTOKEN = 'doAccessToken';
-    
-    const CHECKOUTDATA = 'doCheckoutData';
-    
-    const TRANSACTION = 'doTransaction';
-
     /**
      * Invokes the service itself
      * 
@@ -42,68 +36,6 @@ class MasterpassServiceTest extends BaseWebTestCase
     public function testInstance()
     {
         $this->assertInstanceOf('\Hoya\MasterpassBundle\Service\MasterpassService', $this->getService());
-    }
-
-    /**
-     * Testing request token
-     * 
-     * @return \Hoya\MasterpassBundle\DTO\RequestTokenResponse
-     */
-    public function testRequestToken()
-    {
-        $rt = $this->getService()->getRequestToken();
-
-        $this->assertInstanceOf('\Hoya\MasterpassBundle\DTO\RequestTokenResponse', $rt);
-        $this->assertGreaterThanOrEqual(40, strlen($rt->requestToken), 'requestToken does not have a valid format');
-        $this->assertGreaterThanOrEqual(40, strlen($rt->oAuthSecret), 'oAuthSecret does not have a valid format');
-        $this->assertEquals(900, $rt->oAuthExpiresIn, 'oAuthExpiresIn does not have a valid value');
-
-        return $rt;
-    }
-
-    /**
-     * Testing shoppingcart
-     * 
-     * @depends testRequestToken
-     */
-    public function testShoppingCart(RequestTokenResponse $requestToken)
-    {
-        $item = new ShoppingcartItem();
-        $item->quantity = 1;
-        $item->imageUrl = 'https://localhost.com';
-        $item->description = 'My test item';
-        $item->setAmount(10.00);
-
-        $cart = new Shoppingcart();
-        $cart->currency = 'USD';
-        $cart->addItem(1, $item);
-
-        $shoppingCartXml = $cart->toXML();
-
-        $cartXml = $this->getService()->postShoppingCartData($requestToken, $shoppingCartXml);
-
-        $this->assertRegExp('<ShoppingCartResponse>', $cartXml, 'Response does not contain ShoppingCartResponse');
-        $this->assertRegExp('<OAuthToken>', $cartXml, 'Response does not contain OAuthToken');
-
-        return $cartXml;
-    }
-    
-    /**
-     * Testing shoppingcart
-     * 
-     * @depends testRequestToken
-     */
-    public function testMerchantInit(RequestTokenResponse $requestToken)
-    {
-        $init = new MerchantInit();
-        $initXml = $init->toXML();
-        
-        $responseXml = $this->getService()->postMerchantInitData($requestToken, $initXml);
-
-        $this->assertRegExp('<MerchantInitializationResponse>', $responseXml, 'Response does not contain MerchantInitializationResponse');
-        $this->assertRegExp('<OAuthToken>', $responseXml, 'Response does not contain OAuthToken');
-
-        return $initXml;
     }
 
     /**
@@ -146,111 +78,5 @@ class MasterpassServiceTest extends BaseWebTestCase
         
         return $callback;
     }
-
-    /**
-     * Test access token return.
-     * 
-     * @depends testCallback
-     */
-    public function testAccessToken(CallbackResponse $callback)
-    {
-        $return = 'oauth_token=c7d33d2c6b6b49dc17db786c73a73b3abcadc43a&oauth_token_secret=399e50ba507a0faa27300ecfb50d55390f51f539';
-
-        $connector = $this->getMockConnector($return, self::ACCESSTOKEN);
-
-        $service = new MasterpassService($connector);
-        $accessTokenResponse = $service->getAccessToken($callback);
-
-        $this->assertInstanceOf('\Hoya\MasterpassBundle\DTO\AccessTokenResponse', $accessTokenResponse);
-        $this->assertEquals('c7d33d2c6b6b49dc17db786c73a73b3abcadc43a', $accessTokenResponse->accessToken, 'accessToken does not have a valid value');
-    }
-
-    public function testCheckoutData()
-    {
-        $return = <<<XML
-<Checkout>
-   <Card>
-      <BrandId>master</BrandId>
-      <BrandName>MasterCard</BrandName>
-      <AccountNumber>5204740009900022</AccountNumber>
-      <BillingAddress>
-         <City>Milpitas</City>
-         <Country>US</Country>
-         <CountrySubdivision>US-CA</CountrySubdivision>
-         <Line1>123 S Main St</Line1>
-         <PostalCode>95035</PostalCode>
-      </BillingAddress>
-      <CardHolderName>Joe test</CardHolderName>
-      <ExpiryMonth>1</ExpiryMonth>
-      <ExpiryYear>2020</ExpiryYear>
-   </Card>
-   <TransactionId>446156154</TransactionId>
-   <Contact>
-      <FirstName>Joe</FirstName>
-      <LastName>test</LastName>
-      <Country>US</Country>
-      <EmailAddress>joe.test@example.com</EmailAddress>
-      <PhoneNumber>6547531792</PhoneNumber>
-   </Contact>
-   <ShippingAddress>
-      <City>tuscaloosa</City>
-      <Country>US</Country>
-      <CountrySubdivision>US-AL</CountrySubdivision>
-      <Line1>123 main street</Line1>
-      <PostalCode>35404</PostalCode>
-      <RecipientName>Joe test</RecipientName>
-      <RecipientPhoneNumber>6547530000</RecipientPhoneNumber>
-   </ShippingAddress>
-   <WalletID>101</WalletID>
-   <ExtensionPoint>
-      <CardVerificationStatus>001</CardVerificationStatus>
-   </ExtensionPoint>
-</Checkout>
-XML;
-        $connector = $this->getMockConnector($return, self::CHECKOUTDATA);
-        $service = new MasterpassService($connector);
-
-        $accessToken = new AccessTokenResponse;
-        $accessToken->checkoutResourceUrl = 'https://sandbox.api.mastercard.com/masterpass/v6/checkout/446156154';
-        $accessToken->accessToken = 'c7d33d2c6b6b49dc17db786c73a73b3abcadc43a';
-
-        $checkoutData = $service->getCheckoutData($accessToken);
-
-        $this->assertRegExp('<Checkout>', $checkoutData, 'Response does not contain Checkout');
-        $this->assertRegExp('<TransactionId>', $checkoutData, 'Response does not contain TransactionId');
-    }
-
-    public function testTransaction()
-    {
-        $return = <<<XML
-<?xml version="1.0" encoding="UTF-8"?>
-<MerchantTransactions>
-   <MerchantTransactions>
-      <TransactionId>449087232</TransactionId>
-      <ConsumerKey>cLb0tKkEJhGTITp_6ltDIibO5Wgbx4rIldeXM_jRd4b0476c!414f4859446c4a366c726a327474695545332b353049303d</ConsumerKey>
-      <Currency>USD</Currency>
-      <OrderAmount>100</OrderAmount>
-      <PurchaseDate>2016-07-27T05:31:45+02:00</PurchaseDate>
-      <TransactionStatus>Success</TransactionStatus>
-      <ApprovalCode>sample</ApprovalCode>
-   </MerchantTransactions>
-</MerchantTransactions>
-XML;
-        $purchase = new \DateTime;
-        $transaction = new Transaction;
-        $transaction->transactionId = 449087232;
-        $transaction->consumerKey = 'cLb0tKkEJhGTITp_6ltDIibO5Wgbx4rIldeXM_jRd4b0476c!414f4859446c4a366c726a327474695545332b353049303d';
-        $transaction->currency = 'USD';
-        $transaction->setAmount(1.00);
-        $transaction->transactionStatus = 'Success';
-        $transaction->setPurchaseDate($purchase);
-        $transaction->approvalCode = 'sample';
-
-        $connector = $this->getMockConnector($return, self::TRANSACTION);
-        $service = new MasterpassService($connector);
-
-        $response = $service->postTransaction($transaction);
-        $this->assertRegExp('<TransactionId>', $response, 'Response does not contain TransactionId');
-    }
-
+    
 }
